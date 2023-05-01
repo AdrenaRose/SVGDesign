0681abf232ab3f8bcf6737c0e7b117fef0ba4520
AdrenaRose <Charlenejparker81@gmail.com>
2023-05-01 16:05:51 -0400

    created inquirer prompts

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const inquirer = require ("inquirer");
+const fs = require ("fs");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Logo Text (up to 3 characters)",
+      name: "text",
+    },
+    {
+      type: "input",
+      message: "Text color (keyword or hexadecimal)",
+      name: "textColor",
+    },
+    {
+      type: "list",
+      message: "Choose a logo shape",
+      choices:["circle", "triangle", "square"],
+      name: "shape",
+    },
+    {
+      type: "list",
+      message: "Logo shape color (keyword or hexadecimal)",
+      name: "shapeColor",
+    },
+  ])
+
+  
